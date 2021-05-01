f4764d49396b4425bb5f202a49e9462af9eeaea1
Samiuddin Mohammed <sami7757@gmail.com>
2021-05-01 16:41:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import csv
+import sys
+
+def hotels():
+  csvData = csv.reader(open('hotels.csv','r'))
+  csvData.__next__() # skip first row
+  state = input("What is the state:\n")
+  costOrRating = input("Cost or Rating: cost/rating\n")
+  operation = input("Operation: cheapest/highest/average\n")
+  hotelCode = 0
+  sum = 0.0
+  count = 0
+  avg = 0.0
+  min = sys.maxsize
+  max = -sys.maxsize - 1
+  for row in csvData: 
+    if state.lower() == 'india' or row[2].lower() == state.lower():
+      hotelCode = row[1]
+      if costOrRating.lower() == "cost":
+        metric = float(row[3])
+      elif costOrRating.lower() == "rating":
+        metric = float(row[4])
+      if operation.lower() == 'cheapest' and metric < min:
+        min = metric
+      elif operation.lower() == 'highest' and metric > max:
+        max = metric
+      elif operation.lower() == 'average':
+        sum = sum + metric
+        count = count + 1
+        avg = sum/count
+        
+  if operation.lower() == 'average':
+    print('Average ' + costOrRating + ' of Hotel in ' + state + ' is ' + str(avg))
+  else:
+    print('Hotel with ' + operation + ' ' + costOrRating + ' in ' + state + ' is ' + str(hotelCode))
+
+
+hotels()
